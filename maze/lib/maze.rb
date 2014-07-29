b4535d0ce7318964a8689624ae82713c88124313
Paul Grayson <pdg@alum.mit.edu>
2014-07-28 22:25:36 -0700

    attempt 2 - hash connections, mutable scores

--- maze/lib/maze.rb
+++ maze/lib/maze.rb
@@ -1,2 +1,42 @@
 class Maze
+  attr_accessor :nodes, :connections
+
+  def initialize(nodes, connections)
+    @nodes = nodes
+    @connections = connections
+  end
+
+  def initial_score(goal)
+    nodes.map { |x| x == goal ? 0 : INF }
+  end
+
+  def closest(node_list, score)
+    return node_list.min_by { |x|
+      score[nodes.index(x)]
+    }
+  end
+
+  def expand(score, node)
+    node_val = score[nodes.index(node)]
+
+    connections[node].each do |neighbor|
+      old_score = score[nodes.index(neighbor)]
+      next if old_score < node_val + 1
+
+      score[nodes.index(neighbor)] = node_val+1
+    end
+  end
+
+  def solve(goal)
+    unchecked_nodes = nodes
+    score = initial_score(goal)
+
+    until unchecked_nodes.empty?
+      closest_node = closest(unchecked_nodes, score)
+      expand(score, closest_node)
+      unchecked_nodes -= [closest_node]
+    end
+
+    score
+  end
 end
--- maze/spec/maze_spec.rb
+++ maze/spec/maze_spec.rb
@@ -1,4 +1,71 @@
 require 'spec_helper'
 
+INF = Float::INFINITY
+
 describe Maze do
+  let(:nodes) { [1,2,3,4] }
+  let(:connections) {
+    { 1 => [2],
+      2 => [1,3,4],
+      3 => [2,4],
+      4 => [2,3] }
+  }
+  let(:maze) { Maze.new(nodes, connections) }
+
+  context "a simple maze" do
+    it "finds the distances to 4" do
+      expect(maze.solve(4)).to eq [2,1,1,0]
+    end
+
+    it "finds the distances to 1" do
+      expect(maze.solve(1)).to eq [0,1,2,2]
+    end
+  end
+
+  describe "#initial_score" do
+    specify "given node 1, it sets that to 0 and others to infinity" do
+      expect(maze.initial_score(1)).to eq [0,INF,INF,INF]
+    end
+
+    specify "given node 2, it sets that to 0 and others to infinity" do
+      expect(maze.initial_score(2)).to eq [INF,0,INF,INF]
+    end
+  end
+
+  describe "#closest_node" do
+    specify "given a trivial case, it returns the only node" do
+      expect(maze.closest([1],[0,0,0,0])).to eq 1
+    end
+
+    specify "given a more interesting case, it returns the node with lowest score" do
+      expect(maze.closest([1,2,3],[3,1,7,0])).to eq 2
+    end
+  end
+
+  describe "#expand" do
+    specify "given a 0 node surrounded by INF, sets neighbors to 1" do
+      score = [1,INF,INF,0]
+      maze.expand(score, 4)
+      expect(score).to eq [1,1,1,0]
+    end
+
+    specify "given a 1 node surrounded by INF, sets neighbors to 2" do
+      score = [1,INF,INF,1]
+      maze.expand(score,4)
+      expect(score).to eq [1,2,2,1]
+    end
+
+    specify "given a different 1 node surrounded by INF, sets neighbors to 2" do
+      score = [1,INF,INF,1]
+      maze.expand(score,1)
+      expect(score).to eq [1,2,INF,1]
+    end
+
+    it "doesn't overwrite lower scores" do
+      score = [1,3,0,1]
+      maze.expand(score, 4)
+      expect(score).to eq [1,2,0,1]
+    end
+  end
+
 end
