00949ea54d610244c6fb124dee0e7fa82dc1ce39
Paul Grayson <pdg@alum.mit.edu>
2014-07-29 08:16:48 -0700

    try 2

--- maze/lib/maze.rb
+++ maze/lib/maze.rb
@@ -1,2 +1,40 @@
 class Maze
+  attr_accessor :nodes, :connections
+  def initialize(nodes, connections)
+    @nodes = nodes
+    @connections = connections
+  end
+
+  def solve(goal)
+    score = initial_score(goal)
+    unchecked_nodes = nodes
+    
+    until unchecked_nodes.empty?
+      node = closest(unchecked_nodes, score)
+      check(score, node)
+      unchecked_nodes -= [node]
+    end
+
+    score
+  end
+
+  def index(node)
+    nodes.index node
+  end
+
+  def initial_score(goal)
+    score = [INF]*nodes.length
+    score[index goal] = 0
+    score
+  end
+
+  def closest(nodes, scores)
+    nodes.min_by { |x| scores[index x] }
+  end
+
+  def check(score, node)
+    connections[node].each do |neighbor|
+      score[index neighbor] = score[index node]+1 if score[index neighbor] > score[index node]
+    end
+  end
 end
